2e13e5831d9de3baab956c0556f2ccce8be2751a
Matthew Pollak <matthewpollak22@gmail.com>
2025-04-15 13:12:12 -0700

    Initial Commit

---
+++ MathGenerator/MathGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGeneratorApp.swift
+//  MathGenerator
+//
+//  Created by Matthew Pollak on 2025-04-15.
+//
+
+import SwiftUI
+
+@main
+struct MathGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathGeneratorTests/MathGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathGeneratorTests.swift
+//  MathGeneratorTests
+//
+//  Created by Matthew Pollak on 2025-04-15.
+//
+
+import Testing
+@testable import MathGenerator
+
+struct MathGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
